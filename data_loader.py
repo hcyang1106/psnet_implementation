47b682673776f030bc75ebee712a41d126e0fcc1
scott870430 <scott19880525@gmail.com>
2020-07-04 01:37:49 +0800

    update PSNet

--- data_loader.py
+++ data_loader.py
@@ -6,6 +6,13 @@ from torch.utils import data
 from plyfile import PlyData, PlyElement
 from random import sample
 
+labelset = {'04225987' : 0, '03790512' : 1, '02954340' : 2, '03261776' : 3, '02958343' : 4, '03001627' : 5,\
+ '03642806' : 6, '04099429' : 7, '03636649' : 8, '04379243' : 9, '03948459' : 10, '03624134' : 11,\
+ '03467517' : 12, '02691156' : 13, '03797390' : 14, '02773838' : 15}
+
+labelcount = {'04225987' : [], '03790512' : [], '02954340' : [], '03261776' : [], '02958343' : [], '03001627' : [],\
+ '03642806' : [], '04099429' : [], '03636649' : [], '04379243' : [], '03948459' : [], '03624134' : [],\
+ '03467517' : [], '02691156' : [], '03797390' : [], '02773838' : []}
 class densepointdataset(data.Dataset):
 	def __init__(self, mode, root = './DensePoint/'):
 
@@ -16,7 +23,7 @@ class densepointdataset(data.Dataset):
 		self.categories = os.listdir(self.root)
 		self.point_cloud_path = []
 		self.point_cloud_label = []
-		
+		self.labelset = dict()
 		for i in json_array:
 			#get categroy folder name
 			for j in self.categories:
@@ -25,12 +32,27 @@ class densepointdataset(data.Dataset):
 					break
 
 			for e in json_array[i]:
-				self.point_cloud_label.append(i)
+				labelcount[i].append(self.root + category + '/ply_file/' + e + '.ply')
+				self.point_cloud_label.append(labelset[i])
 				self.point_cloud_path.append(self.root + category + '/ply_file/' + e + '.ply')
 
+		self.sample_data = []
+		self.sample_label = []
+		for e in labelcount.keys():
+			if len(labelcount[e]) > 320:
+				self.sample_data += sample(labelcount[e], 320)
+				self.sample_label += [labelset[e]] * 320
+			else:
+				self.sample_data += labelcount[e]
+				self.sample_label += [labelset[e]] * len(labelcount[e])
+		'''
 		self.point_cloud_label = self.point_cloud_label[:3]
 		self.point_cloud_path = self.point_cloud_path[:3]
+		self.sample_data = self.sample_data[:3]
+		self.sample_label = self.sample_label[:3]
+		'''
 		self.min_point = 39690
+		self.min_point = 5000
 		self.max_point = 40370
 		'''
 		for e in self.point_cloud_path:
@@ -44,35 +66,50 @@ class densepointdataset(data.Dataset):
 		'''
 
 	def __len__(self):
-		return len(self.point_cloud_label)
+
+		#return len(self.sample_label)
+		#return len(self.point_cloud_label)
+		if self.mode == 'train':
+			return len(self.sample_label)
+		elif self.mode == 'test':
+			return len(self.point_cloud_label)
 
 	def __getitem__(self, index):
 		
-		point_cloud = PlyData.read(self.point_cloud_path[index])
-		point_cloud_info = []
+		if self.mode == 'train':
+			point_cloud = PlyData.read(self.sample_data[index])
+		elif self.mode == 'test':
+			point_cloud = PlyData.read(self.point_cloud_path[index])
+
+		point_cloud_temp = []
 		#six dim XYZ RGB
 		#normalize to 1 ~ -1
-		min_max = np.asarray([[2e9, 2e9, 2e9], [0, 0 , 0]])
 		for e in point_cloud.elements[0]:
+			point_cloud_temp.append([e[i] for i in range(0, 6)])
+
+		point_cloud_info = []
+		point_cloud_sample = sample(point_cloud_temp, self.min_point)
+		label = int(self.point_cloud_label[index])
+		min_max = np.asarray([[2e9, 2e9, 2e9], [0, 0 , 0]])
+		for e in point_cloud_sample :
 			for i in range(0, 3):
 				if e[i] > min_max[1][i]:
 					min_max[1][i] = e[i]
 				if e[i] < min_max[0][i]:
 					min_max[0][i] = e[i]
 
-		for e in point_cloud.elements[0]:	
+		for e in point_cloud_sample:	
 			temp = [(e[i] - min_max[0][i]) / (min_max[1][i] - min_max[0][i]) * 2 - 1 for i in range(0, 3)]
 			temp+=[(e[i]/255.0 - 0.5) *2 for i in range(3, 6)]
 			point_cloud_info.append(temp)
-		
-		point_cloud_info = sample(point_cloud_info, self.min_point)
+
 		point_cloud_info = np.asarray(point_cloud_info).T
-		label = int(self.point_cloud_label[index])
+
 
 		return point_cloud_info, label
 
 
 if __name__ == '__main__':
 
-	test = densepointdataset('test')
+	test = densepointdataset('train')
 	test.__getitem__(10)
--- main.py
+++ main.py
@@ -31,10 +31,9 @@ if __name__ == '__main__':
 	model = PSNet.PSNET()
 	optimizer = optim.Adam(model.parameters(), lr = lr, betas=(0.9, 0.999))
 
-	'''
 	train_loader = Data.DataLoader(data_loader.densepointdataset(mode = 'train'),\
 	 batch_size = Batch_size, shuffle = True)#, collate_fn = my_collate)
-	'''
+	
 	test_loader = Data.DataLoader(data_loader.densepointdataset(mode = 'test'), \
 	 batch_size = Batch_size, shuffle = True)#, collate_fn = my_collate)
 
@@ -46,12 +45,12 @@ if __name__ == '__main__':
 	max_test_acc = 0
 	#start training
 	for epoch in range(1,epochs+1):
-		
+		print("epoch: ", epoch)
 		train_label = []
 		train_predict = []
 		#train
 		model.train()
-		for batch, (data, label) in enumerate(test_loader, 1):
+		for batch, (data, label) in enumerate(train_loader, 1):
 			data, label = data.to(device).float(), label.to(device).long()
 			#label = label.to(device)
 			optimizer.zero_grad()
@@ -63,7 +62,6 @@ if __name__ == '__main__':
 			for e in label:
 				train_label.append(e.item())
 
-			print(predict.shape, label.shape)
 			loss = Loss(output, label)
 
 			loss.backward()
@@ -80,7 +78,7 @@ if __name__ == '__main__':
 		with torch.no_grad():
 
 			for (data, label) in test_loader:   
-				data, label = data.to(device), label.to(device)
+				data, label = data.to(device).float(), label.to(device).long()
 
 				output = model(data)
 				predict = output.data.max(1)[1]
@@ -91,6 +89,8 @@ if __name__ == '__main__':
 					test_label.append(e.item())
 
 		test_acc = accuracy_score(test_label, test_predict)
-		Visualization.plot_confusion_matrix(test_label, test_predict, labels=classes, \
-			normalize = cm_normalize, cmap = plt.cm.Blues, title = use_model + ' ' + name, epoch = epoch)
+		print("test accuracy:", test_acc)
 		max_test_acc = max(max_test_acc, test_acc)
+
+		if epoch % 5 == 0:
+			torch.save(model, './save_model/PSNet_' + str(epoch))
